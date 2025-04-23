468b87f12683e6d9c1d1bb9e28cc534e67550b89
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-04-23 18:00:31 -0400

    Update translation_cleanup.sql

--- translation_cleanup.sql
+++ translation_cleanup.sql
@@ -1,1 +1,4 @@
-
+--Select sales person name from those common to the set of bowlers who have earned revenue over 20000 this quarter and last quarter
+Select SalesData.SalesFullName
+FROM
+  (SELECT DISTINCT Person.PersonId
