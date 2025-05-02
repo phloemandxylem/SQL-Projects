ab67fa7605e1dfce34b65d80de60e818482a1817
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-05-02 15:14:49 -0400

    Update translation_cleanup.sql

--- translation_cleanup.sql
+++ translation_cleanup.sql
@@ -5,25 +5,25 @@ FROM
     (Bowlers.BowlerLastName || '. ' ||
     Bowlers.BowerlFirstName) AS BowlerFullName
   FROM ((Bowlers
-  INNER JOIN Bowler_Scores
-  ON Bowlers.BowlerID = Bowler_Scores.BowlerID)
+    INNER JOIN Bowler_Scores
+    ON Bowlers.BowlerID = Bowler_Scores.BowlerID)
     INNER JOIN Tourney_Matches
       ON Tourney_Matches.MatchID = Bowler_Scores.MatchID)
-  INNER JOIN Tournaments
-    ON Tournametns.TourneyID = Tourney_Matches.ToutneyID
+      INNER JOIN Tournaments
+      ON Tournametns.TourneyID = Tourney_Matches.ToutneyID
   WHERE Tournaments.TourneyLocation = 'Thunderbird Lanes'
-  AND Bowler_Scores.Rawscore >= 170)
-AS BowlerTbird INNER JOIN
-  (SELECT DISTINCT Bowlers.BowlerID, 
+    AND Bowler_Scores.Rawscore >= 170)
+    AS BowlerTbird INNER JOIN
+      (SELECT DISTINCT Bowlers.BowlerID, 
     (Bowlers.BowlerLastName || '. ' || Bowlers.BowlerFirstName) AS
-BowlerFullName
-    FROM ((Bowlers
+    BowlerFullName
+  FROM ((Bowlers
     INNER JOIN Bowler_Scores
     ON Tourney_Matches.MatchID = Bowler_Scores MatchID) 
     INNER JOIN Tournaments
     ON Tournaments.TourneyID = 
      Tourney_Matches.TourneyID
-   WHERE Tournametns.TourneyLocation = 'Bolero Lanes' 
+  WHERE Tournametns.TourneyLocation = 'Bolero Lanes' 
    AND Bowler_Scores.RawScore >= 170)
-  AS BowlerBolero 
-  ON BowlerTbird.BowlerID = BowlerBolero.BowlerID
+    AS BowlerBolero 
+    ON BowlerTbird.BowlerID = BowlerBolero.BowlerID
