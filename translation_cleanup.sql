7bfd893b234976453957a1e1cb6297ed6daf0c88
phloemandxylem <30699404+phloemandxylem@users.noreply.github.com>
2025-05-02 15:13:44 -0400

    Update translation_cleanup.sql

--- translation_cleanup.sql
+++ translation_cleanup.sql
@@ -1,4 +1,29 @@
---Select sales person name from those common to the set of bowlers who have earned revenue over 20000 this quarter and last quarter
-Select SalesData.SalesFullName
+--Select bowler full name from those common to the set of bowlers who have a score of 170 or better at Thunderbird lanes and the set of bowlers who have a score of 170 or better at Bolero lanes.
+Select BowlerTbird.BowlerFullName
 FROM
-  (SELECT DISTINCT Person.PersonId
+  (SELECT DISTINCT Bowlers.BowlerID, 
+    (Bowlers.BowlerLastName || '. ' ||
+    Bowlers.BowerlFirstName) AS BowlerFullName
+  FROM ((Bowlers
+  INNER JOIN Bowler_Scores
+  ON Bowlers.BowlerID = Bowler_Scores.BowlerID)
+    INNER JOIN Tourney_Matches
+      ON Tourney_Matches.MatchID = Bowler_Scores.MatchID)
+  INNER JOIN Tournaments
+    ON Tournametns.TourneyID = Tourney_Matches.ToutneyID
+  WHERE Tournaments.TourneyLocation = 'Thunderbird Lanes'
+  AND Bowler_Scores.Rawscore >= 170)
+AS BowlerTbird INNER JOIN
+  (SELECT DISTINCT Bowlers.BowlerID, 
+    (Bowlers.BowlerLastName || '. ' || Bowlers.BowlerFirstName) AS
+BowlerFullName
+    FROM ((Bowlers
+    INNER JOIN Bowler_Scores
+    ON Tourney_Matches.MatchID = Bowler_Scores MatchID) 
+    INNER JOIN Tournaments
+    ON Tournaments.TourneyID = 
+     Tourney_Matches.TourneyID
+   WHERE Tournametns.TourneyLocation = 'Bolero Lanes' 
+   AND Bowler_Scores.RawScore >= 170)
+  AS BowlerBolero 
+  ON BowlerTbird.BowlerID = BowlerBolero.BowlerID
